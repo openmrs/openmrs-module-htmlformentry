6da8cc797d8fbb6777adf1dde26da4b80d5dfb65
Mark Goodrich <mgoodrich@pih.org>
2010-05-25 18:13:31 +0000

    htmlformentry: added uuids and other base openmrs metadata to htmlform object; updated version number to 1.6.4;

--- src/org/openmrs/module/htmlformentry/HtmlForm.java
+++ src/org/openmrs/module/htmlformentry/HtmlForm.java
@@ -1,39 +1,19 @@
 package org.openmrs.module.htmlformentry;
 
-import java.util.Date;
-
+import org.openmrs.BaseOpenmrsMetadata;
 import org.openmrs.Form;
-import org.openmrs.User;
 
 /**
  * The basic HTML Form data object
  */
 
-public class HtmlForm {
+public class HtmlForm extends BaseOpenmrsMetadata {
 	
 	/** Unique identifying id */
     private Integer id;
     
     /** The Form object this HTML Form is associated with */
     private Form form;
-    
-    /** The name of the HTML Form */
-    private String name;
-    
-    /** User that created the HTML Form */
-    private User creator;
-    
-    /** Date the HTML Form was created */
-    private Date dateCreated;
-    
-    /** User that last changed the HTML Form */
-    private User changedBy;
-    
-    /** Date the HTML Form was last changed */
-    private Date dateChanged;
-    
-    /** True/false whether or not the HTML Form has been retired */
-    private Boolean retired = false;
     
     /** Actual XML content of the form */
     private String xmlData;
@@ -60,16 +40,6 @@ public class HtmlForm {
         this.form = form;
     }
 
-    /** Gets the name of this HTML Form */
-    public String getName() {
-        return name;
-    }
-
-    /** Sets the name of this HTML Form */
-    public void setName(String name) {
-        this.name = name;
-    }
-
     /** Gets the actual XML content of the form */
     public String getXmlData() {
         return xmlData;
@@ -78,56 +48,5 @@ public class HtmlForm {
     /** Sets the actual XML content of the form */
     public void setXmlData(String xmlData) {
         this.xmlData = xmlData;
-    }
-    
-    /** Gets the User that created the HTML Form */
-    public User getCreator() {
-        return creator;
-    }
-
-    /** Sets the User that created the HTML Form */
-    public void setCreator(User creator) {
-        this.creator = creator;
-    }
-
-    /** Gets the Date the HTML Form was created */
-    public Date getDateCreated() {
-        return dateCreated;
-    }
-
-    /** Sets the Date the HTML Form was created */
-    public void setDateCreated(Date dateCreated) {
-        this.dateCreated = dateCreated;
-    }
-
-    /** Gets the User that last changed the HTML Form */
-    public User getChangedBy() {
-        return changedBy;
-    }
-
-    /** Sets the User that last changed the HTML Form */
-    public void setChangedBy(User changedBy) {
-        this.changedBy = changedBy;
-    }
-
-    /** Gets the Date the HTML Form was last changed */
-    public Date getDateChanged() {
-        return dateChanged;
-    }
-
-    /** Sets the Date the HTML Form was last changed */
-    public void setDateChanged(Date dateChanged) {
-        this.dateChanged = dateChanged;
-    }
-
-    /** Gets whether or not the HTML Form has been retired */
-    public Boolean getRetired() {
-        return retired;
-    }
-
-    /** Sets whether or not the HTML Form has been retired */
-    public void setRetired(Boolean retired) {
-        this.retired = retired;
-    }
-       
+    }       
 }
