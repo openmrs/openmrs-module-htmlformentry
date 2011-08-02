3f38914958970b5bccd701dca23542c97cc8f708
David Thomas <pihdave@gmail.com>
2011-08-02 20:33:05 +0000

    HTML-149.  Adding attribute descriptor to Relationship handler so that RelationshipTypes get picked up by metadata sharing.

--- src/org/openmrs/module/htmlformentry/handler/RelationshipTagHandler.java
+++ src/org/openmrs/module/htmlformentry/handler/RelationshipTagHandler.java
@@ -1,10 +1,13 @@
 package org.openmrs.module.htmlformentry.handler;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
+import org.openmrs.RelationshipType;
 import org.openmrs.module.htmlformentry.FormEntrySession;
 import org.openmrs.module.htmlformentry.FormSubmissionController;
-import org.openmrs.module.htmlformentry.element.DrugOrderSubmissionElement;
 import org.openmrs.module.htmlformentry.element.RelationshipSubmissionElement;
 
 /**
@@ -12,6 +15,14 @@ import org.openmrs.module.htmlformentry.element.RelationshipSubmissionElement;
  */
 public class RelationshipTagHandler extends SubstitutionTagHandler {
 
+	
+	protected List<AttributeDescriptor> createAttributeDescriptors() {
+		List<AttributeDescriptor> attributeDescriptors = new ArrayList<AttributeDescriptor>();
+		attributeDescriptors.add(new AttributeDescriptor("type", RelationshipType.class));
+		return Collections.unmodifiableList(attributeDescriptors);
+	}
+	
+	
     @Override
     protected String getSubstitution(FormEntrySession session, FormSubmissionController controllerActions,
             Map<String, String> parameters) {
