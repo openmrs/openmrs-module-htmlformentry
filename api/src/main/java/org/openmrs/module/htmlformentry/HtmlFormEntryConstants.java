74f4c9fcf3ffcd85b9fa22a69374985f27cce25e
Daniel Kayiwa <dkayiwa@openmrs.org>
2012-02-03 18:33:14 +0300

    Remove default static text (dd/mm/yyyy) for date obs - HTML-218

--- api/src/main/java/org/openmrs/module/htmlformentry/HtmlFormEntryConstants.java
+++ api/src/main/java/org/openmrs/module/htmlformentry/HtmlFormEntryConstants.java
@@ -21,4 +21,6 @@ public class HtmlFormEntryConstants {
     public final static String[] PATIENT_TAGS = {"patient"};
 
     public final static String GP_DATE_FORMAT = "htmlformentry.dateFormat";
+    
+    public final static String GP_SHOW_DATE_FORMAT = "htmlformentry.showDateFormat";
 }
