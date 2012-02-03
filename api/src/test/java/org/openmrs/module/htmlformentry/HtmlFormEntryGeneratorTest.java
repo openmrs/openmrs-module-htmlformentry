cbbd4a14f5142bd4a78fd460f635d06900d83e59
rkorytkowski <rafal@openmrs.org>
2012-02-03 17:03:26 +0100

    HTML-286: Add master div to an HTML Form https://tickets.openmrs.org/browse/HTML-286

--- api/src/test/java/org/openmrs/module/htmlformentry/HtmlFormEntryGeneratorTest.java
+++ api/src/test/java/org/openmrs/module/htmlformentry/HtmlFormEntryGeneratorTest.java
@@ -12,83 +12,80 @@ import org.openmrs.test.BaseModuleContextSensitiveTest;
 import org.openmrs.test.Verifies;
 
 public class HtmlFormEntryGeneratorTest extends BaseModuleContextSensitiveTest {
-
+	
 	protected final Log log = LogFactory.getLog(getClass());
-
+	
 	protected static final String XML_DATASET_PATH = "org/openmrs/module/htmlformentry/include/";
 	
 	protected static final String XML_HTML_FORM_ENTRY_TEST_DATASET = "htmlFormEntryTestDataSet";
-
+	
 	private Patient patient = null;
-
+	
 	@Before
 	public void setupDatabase() throws Exception {
 		executeDataSet(XML_DATASET_PATH + new TestUtil().getTestDatasetFilename(XML_HTML_FORM_ENTRY_TEST_DATASET));
 		patient = Context.getPatientService().getPatient(2);
 	}
-
+	
 	/**
 	 * @see {@link HtmlFormEntryGenerator#applyExcludes(FormEntrySession,String)}
-	 * 
 	 */
 	@Test
 	@Verifies(value = "should return correct xml after apply excludeIf tag", method = "applyExcludes(FormEntrySession,String)")
-	public void applyExcludes_shouldReturnCorrectXmlAfterApplyExcludeIfTag()
-			throws Exception {
+	public void applyExcludes_shouldReturnCorrectXmlAfterApplyExcludeIfTag() throws Exception {
 		LogicUtil.registerDefaultRules();
 		String htmlform = "<htmlform><excludeIf logicTest=\"GENDER = F\">This shows a logic test for a woman</excludeIf><excludeIf logicTest=\"GENDER = M\">This shows a logic test for a man</excludeIf></htmlform>";
 		FormEntrySession session = new FormEntrySession(patient, htmlform);
-		Assert.assertEquals(
-				"<htmlform>This shows a logic test for a woman</htmlform>",
-				session.getHtmlToDisplay());
+		Assert.assertEquals("<div class=\"htmlform\">This shows a logic test for a woman</div>", session.getHtmlToDisplay());
 	}
-
+	
 	/**
 	 * @see {@link HtmlFormEntryGenerator#applyIncludes(FormEntrySession,String)}
-	 * 
 	 */
 	@Test
 	@Verifies(value = "should return correct xml after apply include tag", method = "applyIncludes(FormEntrySession,String)")
-	public void applyIncludes_shouldReturnCorrectXmlAfterApplyIncludeTag()
-			throws Exception {
+	public void applyIncludes_shouldReturnCorrectXmlAfterApplyIncludeTag() throws Exception {
 		LogicUtil.registerDefaultRules();
 		String htmlform = "<htmlform><includeIf logicTest=\"GENDER = F\">This shows a logic test for a woman</includeIf><includeIf logicTest=\"GENDER = M\">This shows a logic test for a man</includeIf></htmlform>";
 		FormEntrySession session = new FormEntrySession(patient, htmlform);
-		Assert.assertEquals(
-				"<htmlform>This shows a logic test for a man</htmlform>",
-				session.getHtmlToDisplay());
+		Assert.assertEquals("<div class=\"htmlform\">This shows a logic test for a man</div>", session.getHtmlToDisplay());
 	}
-
+	
 	/**
 	 * @see {@link HtmlFormEntryGenerator#getTestStr(String)}
-	 * 
 	 */
 	@Test
 	@Verifies(value = "should extract the correct expression from teststr", method = "getTestStr(String)")
-	public void getTestStr_shouldExtractTheCorrectExpressionFromTeststr()
-			throws Exception {
+	public void getTestStr_shouldExtractTheCorrectExpressionFromTeststr() throws Exception {
 		String includeStr = "logicTest = \"GENDER = F\">";// <includeIf test =
 		// "FEMALE">
-		Assert.assertEquals("GENDER = F", HtmlFormEntryGenerator
-				.getTestStr(includeStr));
+		Assert.assertEquals("GENDER = F", HtmlFormEntryGenerator.getTestStr(includeStr));
 		includeStr = "velocityTest = \"\"$patient.gender\" == \"F\"\">";
-		Assert.assertEquals("\"$patient.gender\" == \"F\"",
-				HtmlFormEntryGenerator.getTestStr(includeStr));
+		Assert.assertEquals("\"$patient.gender\" == \"F\"", HtmlFormEntryGenerator.getTestStr(includeStr));
 	}
-
+	
 	/**
 	 * @see {@link HtmlFormEntryGenerator#processIncludeLogic(FormEntrySession,String)}
-	 * 
 	 */
 	@Test
 	@Verifies(value = "should return a correct boolean value for logic test string", method = "processIncludeLogic(FormEntrySession,String)")
-	public void processIncludeLogic_shouldReturnACorrectBooleanValueForLogicTestString()
-			throws Exception {
+	public void processIncludeLogic_shouldReturnACorrectBooleanValueForLogicTestString() throws Exception {
 		LogicUtil.registerDefaultRules();
 		String htmlform = "<htmlform><includeIf logicTest=\"GENDER = F\">This shows a logic test for a woman</includeIf><includeIf logicTest=\"GENDER = M\">This shows a logic test for a man</includeIf></htmlform>";
 		String testStr = "logicTest=\"GENDER = M\">";
 		FormEntrySession session = new FormEntrySession(patient, htmlform);
-		Assert.assertEquals(true, HtmlFormEntryGenerator.processIncludeLogic(
-				session, testStr));
+		Assert.assertEquals(true, HtmlFormEntryGenerator.processIncludeLogic(session, testStr));
+	}
+	
+	/**
+	 * @see HtmlFormEntryGenerator#wrapInDiv(String)
+	 * @verifies remove htmlform tag and wrap form in div
+	 */
+	@Test
+	public void wrapInDiv_shouldRemoveHtmlformTagAndWrapFormInDiv() throws Exception {
+		String htmlform = "<htmlform>someContent</htmlform>";
+		HtmlFormEntryGenerator generator = new HtmlFormEntryGenerator();
+		htmlform = generator.wrapInDiv(htmlform);
+		Assert.assertEquals("<div class=\"htmlform\">someContent</div>", htmlform);
 	}
 }
