42306e407a3e749e16ddeda5a207ed11ba4def3a
wyclif <wyclif@openmrs.org>
2012-04-11 21:11:49 -0400

    Applying auto format changes

--- api/src/main/java/org/openmrs/module/htmlformentry/widget/LocationWidget.java
+++ api/src/main/java/org/openmrs/module/htmlformentry/widget/LocationWidget.java
@@ -9,74 +9,78 @@ import javax.servlet.http.HttpServletRequest;
 import org.openmrs.Location;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.htmlformentry.FormEntryContext;
-import org.openmrs.module.htmlformentry.HtmlFormEntryUtil;
 import org.openmrs.module.htmlformentry.FormEntryContext.Mode;
+import org.openmrs.module.htmlformentry.HtmlFormEntryUtil;
 import org.springframework.util.StringUtils;
 
 /**
- * A widget that allows for the selection of a Location. Implemented using a drop-down selection list.
+ * A widget that allows for the selection of a Location. Implemented using a drop-down selection
+ * list.
  */
 public class LocationWidget implements Widget {
-   
-    private Location location;
-    private List<Location> options;
-    
-    public LocationWidget() { }
-
-    @Override
-    public String generateHtml(FormEntryContext context) {
-        if (context.getMode() == Mode.VIEW) {
-            if (location != null)
-                return WidgetFactory.displayValue(location.getName());
-            else
-                return "";
-        }
-           
-        StringBuilder sb = new StringBuilder();
-        sb.append("<select id=\"" + context.getFieldName(this) + "\" name=\"" + context.getFieldName(this) + "\">");
-        sb.append("\n<option value=\"\">");
-        sb.append(Context.getMessageSourceService().getMessage("htmlformentry.chooseALocation"));
-        sb.append("</option>");
-        List<Location> useLocations;
-        if (options != null) {
-            useLocations = options;
-        } else {
-            useLocations = Context.getLocationService().getAllLocations();
-            Collections.sort(useLocations, new Comparator<Location>() {
-                @Override
-                public int compare(Location left, Location right) {
-                    return left.getName().compareTo(right.getName());
-                }
-            });
-        }
-        for (Location l : useLocations) {
-            sb.append("\n<option");
-            if (location != null && location.equals(l))
-                sb.append(" selected=\"true\"");
-            sb.append(" value=\"" + l.getLocationId() + "\">").append(l.getName()).append("</option>");
-        }
-        sb.append("</select>");
-        return sb.toString();
-    }
-
-    @Override
-    public Object getValue(FormEntryContext context, HttpServletRequest request) {
-        String val = request.getParameter(context.getFieldName(this));
-        if (StringUtils.hasText(val))
-            return HtmlFormEntryUtil.convertToType(val, Location.class);
-        return null;
-    }
-
-    @Override
-    public void setInitialValue(Object initialValue) {
-        location = (Location) initialValue;
-    }
-
-    /**
-     * Sets the Locations to use as options for this widget
-     */
-    public void setOptions(List<Location> options) {
-        this.options = options;        
-    }
-    
+	
+	private Location location;
+	
+	private List<Location> options;
+	
+	public LocationWidget() {
+	}
+	
+	@Override
+	public String generateHtml(FormEntryContext context) {
+		if (context.getMode() == Mode.VIEW) {
+			if (location != null)
+				return WidgetFactory.displayValue(location.getName());
+			else
+				return "";
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("<select id=\"" + context.getFieldName(this) + "\" name=\"" + context.getFieldName(this) + "\">");
+		sb.append("\n<option value=\"\">");
+		sb.append(Context.getMessageSourceService().getMessage("htmlformentry.chooseALocation"));
+		sb.append("</option>");
+		List<Location> useLocations;
+		if (options != null) {
+			useLocations = options;
+		} else {
+			useLocations = Context.getLocationService().getAllLocations();
+			Collections.sort(useLocations, new Comparator<Location>() {
+				
+				@Override
+				public int compare(Location left, Location right) {
+					return left.getName().compareTo(right.getName());
+				}
+			});
+		}
+		for (Location l : useLocations) {
+			sb.append("\n<option");
+			if (location != null && location.equals(l))
+				sb.append(" selected=\"true\"");
+			sb.append(" value=\"" + l.getLocationId() + "\">").append(l.getName()).append("</option>");
+		}
+		sb.append("</select>");
+		return sb.toString();
+	}
+	
+	@Override
+	public Object getValue(FormEntryContext context, HttpServletRequest request) {
+		String val = request.getParameter(context.getFieldName(this));
+		if (StringUtils.hasText(val))
+			return HtmlFormEntryUtil.convertToType(val, Location.class);
+		return null;
+	}
+	
+	@Override
+	public void setInitialValue(Object initialValue) {
+		location = (Location) initialValue;
+	}
+	
+	/**
+	 * Sets the Locations to use as options for this widget
+	 */
+	public void setOptions(List<Location> options) {
+		this.options = options;
+	}
+	
 }
