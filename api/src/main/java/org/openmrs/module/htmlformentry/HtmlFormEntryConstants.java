9f110a89947bb5a8437ba24a0d2c50de8c0d0cd4
Darius Jazayeri <jazayeri@alum.mit.edu>
2012-07-02 18:32:03 -0700

    HTML-364 Support an option that lets you export HTML Forms via metadatasharing without including any concepts as dependencies

--- api/src/main/java/org/openmrs/module/htmlformentry/HtmlFormEntryConstants.java
+++ api/src/main/java/org/openmrs/module/htmlformentry/HtmlFormEntryConstants.java
@@ -23,4 +23,6 @@ public class HtmlFormEntryConstants {
     public final static String GP_DATE_FORMAT = "htmlformentry.dateFormat";
     
     public final static String GP_SHOW_DATE_FORMAT = "htmlformentry.showDateFormat";
+    
+    public final static String GP_CLASSES_NOT_TO_EXPORT_WITH_MDS = "htmlformentry.classesNotToExportWithMetadataSharing";
 }
