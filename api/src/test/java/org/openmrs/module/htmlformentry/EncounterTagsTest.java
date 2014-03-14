e31395026470c9b42e8da8455906b3ecc4cc1e1c
mogoodrich <mgoodrich@pih.org>
2014-03-14 14:32:07 -0400

    HTML-482: Set the id of the encounterDate, encouterLocation, and encounterProvider fieldse

--- api/src/test/java/org/openmrs/module/htmlformentry/EncounterTagsTest.java
+++ api/src/test/java/org/openmrs/module/htmlformentry/EncounterTagsTest.java
@@ -22,32 +22,77 @@ public class EncounterTagsTest extends BaseModuleContextSensitiveTest {
 		patient = Context.getPatientService().getPatient(2);
 		LogicUtil.registerDefaultRules();
 	}
-	
+
+    @Test
+    public void shouldDisplayDatePropertyAccessor() throws Exception {
+        String htmlform = "<htmlform><encounterDate/></htmlform>";
+        FormEntrySession session = new FormEntrySession(patient, htmlform, null);
+        session.getHtmlToDisplay();
+        TestUtil.assertFuzzyContains("propertyAccessorInfo\\['encounterDate.value'\\]", session.getFieldAccessorJavascript());
+        TestUtil.assertFuzzyContains("propertyAccessorInfo\\['encounterDate.error'\\]", session.getFieldAccessorJavascript());
+    }
+
+    @Test
+    public void shouldDisplayProviderValuePropertyAccessor() throws Exception {
+        String htmlform = "<htmlform><encounterProvider/></htmlform>";
+        FormEntrySession session = new FormEntrySession(patient, htmlform, null);
+        session.getHtmlToDisplay();
+        TestUtil.assertFuzzyContains("propertyAccessorInfo\\['encounterProvider.value'\\]", session.getFieldAccessorJavascript());
+        TestUtil.assertFuzzyContains("propertyAccessorInfo\\['encounterProvider.error'\\]", session.getFieldAccessorJavascript());
+    }
+
+    @Test
+    public void shouldDisplayLocationValuePropertyAccessor() throws Exception {
+        String htmlform = "<htmlform><encounterLocation/></htmlform>";
+        FormEntrySession session = new FormEntrySession(patient, htmlform, null);
+        session.getHtmlToDisplay();
+        TestUtil.assertFuzzyContains("propertyAccessorInfo\\['encounterLocation.value'\\]", session.getFieldAccessorJavascript());
+        TestUtil.assertFuzzyContains("propertyAccessorInfo\\['encounterLocation.error'\\]", session.getFieldAccessorJavascript());
+    }
+
+    @Test
+    public void shouldDisplayEncounterTypeValue() throws Exception {
+        String htmlform = "<htmlform><encounterType/></htmlform>";
+        FormEntrySession session = new FormEntrySession(patient, htmlform, null);
+        session.getHtmlToDisplay();
+        TestUtil.assertFuzzyContains("propertyAccessorInfo\\['encounterType.value'\\]", session.getFieldAccessorJavascript());
+        TestUtil.assertFuzzyContains("propertyAccessorInfo\\['encounterType.error'\\]", session.getFieldAccessorJavascript());
+    }
+
 	@Test
-	public void shouldDisplayDatePropertyAccessor() throws Exception {
+	public void shouldDisplayDatePropertyAccessorUsingCustomName() throws Exception {
 	String htmlform = "<htmlform><encounterDate id=\"myEncounter\"/></htmlform>";
 		FormEntrySession session = new FormEntrySession(patient, htmlform, null);
         session.getHtmlToDisplay();
 		TestUtil.assertFuzzyContains("propertyAccessorInfo\\['myEncounter.value'\\]", session.getFieldAccessorJavascript());
 		TestUtil.assertFuzzyContains("propertyAccessorInfo\\['myEncounter.error'\\]", session.getFieldAccessorJavascript());
 	}
-	
+
 	@Test
-	public void shouldDisplayProviderValuePropertyAccessor() throws Exception {
+	public void shouldDisplayProviderValuePropertyAccessorUsingCustomName() throws Exception {
 	String htmlform = "<htmlform><encounterProvider id=\"myEncounter\"/></htmlform>";
 		FormEntrySession session = new FormEntrySession(patient, htmlform, null);
         session.getHtmlToDisplay();
 		TestUtil.assertFuzzyContains("propertyAccessorInfo\\['myEncounter.value'\\]", session.getFieldAccessorJavascript());
 		TestUtil.assertFuzzyContains("propertyAccessorInfo\\['myEncounter.error'\\]", session.getFieldAccessorJavascript());
 	}
-	
+
 	@Test
-	public void shouldDisplayLocationValuePropertyAccessor() throws Exception {
+	public void shouldDisplayLocationValuePropertyAccessorUsingCustomName() throws Exception {
 	String htmlform = "<htmlform><encounterLocation id=\"myEncounter\"/></htmlform>";
 		FormEntrySession session = new FormEntrySession(patient, htmlform, null);
         session.getHtmlToDisplay();
 		TestUtil.assertFuzzyContains("propertyAccessorInfo\\['myEncounter.value'\\]", session.getFieldAccessorJavascript());
 		TestUtil.assertFuzzyContains("propertyAccessorInfo\\['myEncounter.error'\\]", session.getFieldAccessorJavascript());
 	}
+
+    @Test
+    public void shouldDisplayEncounterTypeValuePropertyAccessorUsingCustomName() throws Exception {
+        String htmlform = "<htmlform><encounterType id=\"myEncounter\"/></htmlform>";
+        FormEntrySession session = new FormEntrySession(patient, htmlform, null);
+        session.getHtmlToDisplay();
+        TestUtil.assertFuzzyContains("propertyAccessorInfo\\['myEncounter.value'\\]", session.getFieldAccessorJavascript());
+        TestUtil.assertFuzzyContains("propertyAccessorInfo\\['myEncounter.error'\\]", session.getFieldAccessorJavascript());
+    }
 	
 }
