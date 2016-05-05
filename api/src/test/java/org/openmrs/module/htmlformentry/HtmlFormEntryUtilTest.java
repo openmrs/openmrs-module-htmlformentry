cfe2491cf1ce3bcff4e3141f7fbfa6aa2c0d5681
Donggun Kim <dkim91@gmail.com>
2016-05-05 18:09:36 -0500

    Added a test for HtmlFormEntryUtil.findDescendant()

---
+++ api/src/test/java/org/openmrs/module/htmlformentry/HtmlFormEntryUtilTest.java
@@ -0,0 +1,28 @@
+package org.openmrs.module.htmlformentry;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openmrs.module.htmlformentry.HtmlFormEntryUtil;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class HtmlFormEntryUtilTest {
+
+    @Test
+    public void testEmptyNodeDescendant() throws Exception {
+        String xml = "<htmlform>\n"
+                + "<obs conceptId=\"2\" />\n"
+                + "</htmlform>";
+        Document document = HtmlFormEntryUtil.stringToDocument(xml);
+        Node obsNode = HtmlFormEntryUtil.findDescendant(document, "obs");
+        Assert.assertNotNull(obsNode);
+    }
+}
