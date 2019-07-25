1b60268d9250926eb7e5eb4676e5113664c63b53
Samuel Male <samuelsmalek@gmail.com>
2019-07-26 00:20:57 +0300

    HTML-705: New <condition/> tag (from Core 2.2).

---
+++ api-2.2/src/main/java/org/openmrs/module/htmlformentry/handler/ConditionTagHandlerSupport2_2.java
@@ -0,0 +1,24 @@
+package org.openmrs.module.htmlformentry.handler;
+
+import java.util.Map;
+
+import org.openmrs.annotation.OpenmrsProfile;
+import org.openmrs.module.htmlformentry.ConditionElement;
+import org.openmrs.module.htmlformentry.FormEntrySession;
+import org.openmrs.module.htmlformentry.FormSubmissionController;
+
+@OpenmrsProfile(openmrsPlatformVersion = "2.2.*")
+public class ConditionTagHandlerSupport2_2 implements ConditionTagHandlerSupport {
+	
+	@Override
+	public String getSubstitution(FormEntrySession session, FormSubmissionController controller, Map<String, String> attributes) {
+		ConditionElement element = new ConditionElement();
+		String required = attributes.get("required");
+		if (required != null) {
+			element.setRequired(required.equalsIgnoreCase("true"));
+		}
+		session.getSubmissionController().addAction(element);
+		return element.generateHtml(session.getContext());	
+	}
+
+}
---
+++ api/src/main/java/org/openmrs/module/htmlformentry/handler/ConditionTagHandler.java
@@ -0,0 +1,21 @@
+package org.openmrs.module.htmlformentry.handler;
+
+import java.util.Map;
+
+import org.openmrs.module.htmlformentry.BadFormDesignException;
+import org.openmrs.module.htmlformentry.FormEntrySession;
+import org.openmrs.module.htmlformentry.FormSubmissionController;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ConditionTagHandler extends SubstitutionTagHandler {
+
+	@Autowired
+	private ConditionTagHandlerSupport handler;
+	
+	@Override
+	protected String getSubstitution(FormEntrySession session, FormSubmissionController controllerActions,
+			Map<String, String> parameters) throws BadFormDesignException {
+		return handler.getSubstitution(session, controllerActions, parameters);
+	}
+
+}
