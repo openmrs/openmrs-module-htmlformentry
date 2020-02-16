f5f72c9ca563cf42a0fc0733be069311c0d097ea
reagan <reaganment@gmail.com>
2020-02-16 01:25:09 -0500

    HTML-26: Allow HTML Forms to be split into pages

--- api/src/main/java/org/openmrs/module/htmlformentry/FormEntrySession.java
+++ api/src/main/java/org/openmrs/module/htmlformentry/FormEntrySession.java
@@ -410,6 +410,7 @@ public class FormEntrySession {
         xml = htmlGenerator.convertSpecialCharactersWithinLogicAndVelocityTests(xml);
         xml = htmlGenerator.applyRoleRestrictions(xml);
         xml = htmlGenerator.applyMacros(this, xml);
+        xml = htmlGenerator.processPages(this,xml);
         xml = htmlGenerator.applyRepeats(xml);
         xml = htmlGenerator.applyTranslations(xml, context);
         xml = htmlGenerator.applyTags(this, xml);
