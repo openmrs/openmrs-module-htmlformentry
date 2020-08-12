1093d562bb99ad7d6ed55a5c2dffc1b4f68f4588
Luis Oliveira <luis.oliveira@icrc.org>
2020-08-12 10:46:09 +0100

    Fixed getControlId method. Added unitary tests for HtmlFormEntryUtil2_3

--- api-2.3/src/main/java/org/openmrs/module/htmlformentry/HtmlFormEntryUtil2_3.java
+++ api-2.3/src/main/java/org/openmrs/module/htmlformentry/HtmlFormEntryUtil2_3.java
@@ -1,6 +1,7 @@
 package org.openmrs.module.htmlformentry;
 
 import org.openmrs.FormRecordable;
+import org.springframework.util.StringUtils;
 
 /**
  * HTML Form Entry utility methods for version 2.3
@@ -15,12 +16,17 @@ public class HtmlFormEntryUtil2_3 {
 	 * @return
 	 */
 	public static String getControlId(FormRecordable openmrsData) {
-		if (openmrsData.getFormFieldPath() == null) {
-			return null;
-		} else {
-			String controlId = openmrsData.getFormFieldPath().split("/")[1];
-			return controlId.split("-")[0];
+		
+		// Validate if getFormFieldPath exists.
+		if (StringUtils.isEmpty(openmrsData.getFormFieldPath())) {
+			throw new IllegalStateException(
+			        "A form recordable object was found to have no form namespace and path set, its control id in the form could not be determined.");
 		}
+		
+		// get the control id
+		String controlId = openmrsData.getFormFieldPath().split("/")[1];
+		return controlId.split("(-)(?!.*-)")[0];
+		
 	}
 	
 }
---
+++ api-2.3/src/test/java/org/openmrs/htmlformentry/HtmlFormEntryUtil2_3Test.java
@@ -0,0 +1,39 @@
+package org.openmrs.htmlformentry;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openmrs.Obs;
+import org.openmrs.module.htmlformentry.HtmlFormEntryUtil2_3;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.openmrs.module.htmlformentry.HtmlFormEntryConstants.FORM_NAMESPACE;
+
+@RunWith(PowerMockRunner.class)
+public class HtmlFormEntryUtil2_3Test {
+	
+	@Test
+	public void getControlId_shouldReturnControlId() {
+		
+		// Prepare parameters
+		Obs observation = new Obs();
+		observation.setFormField(FORM_NAMESPACE, "MyForm.1.0/my-condition-tag-0");
+		
+		// Test
+		String controlId = HtmlFormEntryUtil2_3.getControlId(observation);
+		
+		// Validation
+		Assert.assertEquals("my-condition-tag", controlId);
+	}
+	
+	@Test(expected = IllegalStateException.class)
+	public void getControlId_shouldThrowIllegalStateException() {
+		
+		// Prepare parameters
+		Obs observation = new Obs();
+		
+		// Test
+		String controlId = HtmlFormEntryUtil2_3.getControlId(observation);
+		
+	}
+}
