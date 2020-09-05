31610a4fee4b5a543eab087ae54da7221115bcb2
dkayiwa <kayiwadaniel@gmail.com>
2020-09-05 23:54:35 +0300

    HTML-725 module should run on platform versions before 2.3.2

--- api/src/main/java/org/openmrs/module/htmlformentry/handler/ConditionTagHandlerSupport1_10.java
+++ api/src/main/java/org/openmrs/module/htmlformentry/handler/ConditionTagHandlerSupport1_10.java
@@ -6,13 +6,13 @@ import org.openmrs.annotation.OpenmrsProfile;
 import org.openmrs.module.htmlformentry.FormEntrySession;
 import org.openmrs.module.htmlformentry.FormSubmissionController;
 
-@OpenmrsProfile(openmrsPlatformVersion = "[1.10 - 2.1.*]")
+@OpenmrsProfile(openmrsPlatformVersion = "[1.10 - 2.3.1]")
 public class ConditionTagHandlerSupport1_10 implements ConditionTagHandlerSupport {
 	
 	@Override
 	public String getSubstitution(FormEntrySession session, FormSubmissionController controllerActions,
 	        Map<String, String> parameters) {
-		throw new RuntimeException("The Condition tag should be used on 2.3.1 platform version and above.");
+		throw new RuntimeException("The Condition tag should be used on 2.3.2 platform version and above.");
 	}
 	
 }
