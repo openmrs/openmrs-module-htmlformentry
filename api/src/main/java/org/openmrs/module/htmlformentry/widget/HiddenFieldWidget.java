c7afae9c58a1dfe17dbf58e2a5959f691ccb9d10
Michael Seaton <mseaton@pih.org>
2020-10-29 13:17:21 -0400

    Revert widget changes that were not ultimately used

--- api/src/main/java/org/openmrs/module/htmlformentry/widget/HiddenFieldWidget.java
+++ api/src/main/java/org/openmrs/module/htmlformentry/widget/HiddenFieldWidget.java
@@ -2,24 +2,17 @@ package org.openmrs.module.htmlformentry.widget;
 
 import javax.servlet.http.HttpServletRequest;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-import org.apache.commons.lang.StringUtils;
 import org.openmrs.module.htmlformentry.FormEntryContext;
 import org.openmrs.module.htmlformentry.FormEntryContext.Mode;
 
 /**
- * A widget that implements a hidden input field like {@code <input type="hidden"/>},
+ * A widget that implements a hidden input field like {@code <input
+ * type="hidden"/>},
  */
 public class HiddenFieldWidget implements Widget {
 	
 	private String initialValue;
-	
-	private String label;
-	
-	private Map<String, String> attributes;
-	
+
 	/**
 	 * Gets the initial value associated with this widget
 	 * 
@@ -41,18 +34,11 @@ public class HiddenFieldWidget implements Widget {
 	public String generateHtml(FormEntryContext context) {
 		StringBuilder sb = new StringBuilder();
 		if (context.getMode() != Mode.VIEW) {
-			sb.append("<input type=\"hidden\" name=\"").append(context.getFieldName(this)).append("\"");
-			sb.append(" id=\"").append(context.getFieldName(this)).append("\"");
-			for (String att : getAttributes().keySet()) {
-				sb.append(" ").append(att).append("=\"").append(getAttributes().get(att)).append("\"");
-			}
-			if (initialValue != null) {
-				sb.append(" value=\"").append(initialValue).append("\"");
-			}
+			sb.append("<input type=\"hidden\" name=\"" + context.getFieldName(this) + "\" id=\"" + context.getFieldName(this)
+			        + "\"");
+			if (initialValue != null)
+				sb.append(" value=\"" + initialValue + "\"");
 			sb.append("/>");
-		}
-		if (StringUtils.isNotBlank(label)) {
-			sb.append(label);
 		}
 		return sb.toString();
 	}
@@ -61,19 +47,5 @@ public class HiddenFieldWidget implements Widget {
 	public Object getValue(FormEntryContext context, HttpServletRequest request) {
 		return request.getParameter(context.getFieldName(this));
 	}
-	
-	public Map<String, String> getAttributes() {
-		if (attributes == null) {
-			attributes = new LinkedHashMap<>();
-		}
-		return attributes;
-	}
-	
-	public void addAttribute(String key, String value) {
-		getAttributes().put(key, value);
-	}
-	
-	public void setLabel(String label) {
-		this.label = label;
-	}
+
 }
