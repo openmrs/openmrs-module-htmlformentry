a293ddf493f0070b6fd3f798085ea0eba36d3f9d
Michael Seaton <mseaton@pih.org>
2020-11-18 21:57:56 -0500

    HTML-765 - Support initial values after they have been removed from the schema

--- api/src/main/java/org/openmrs/module/htmlformentry/widget/SingleOptionWidget.java
+++ api/src/main/java/org/openmrs/module/htmlformentry/widget/SingleOptionWidget.java
@@ -4,8 +4,14 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.openmrs.Concept;
+import org.openmrs.OpenmrsData;
+import org.openmrs.OpenmrsMetadata;
 import org.openmrs.OpenmrsObject;
 import org.openmrs.module.htmlformentry.FormEntryContext;
+import org.openmrs.module.htmlformentry.HtmlFormEntryUtil;
+import org.openmrs.module.htmlformentry.element.ValueStub;
+import org.openmrs.util.OpenmrsUtil;
 
 /**
  * This represents a single widget on a form which presents several coded options, of which only one
@@ -36,6 +42,57 @@ public abstract class SingleOptionWidget implements Widget {
 			} else {
 				this.initialValue = initialValue.toString();
 			}
+			addOptionIfMissing(initialValue);
+		}
+	}
+	
+	/**
+	 * The purpose of this method is to determine if the initial value configured on the widget is among
+	 * the options configured for the widget. If it is not among the options, then add it to the
+	 * available options, in order to ensure that existing forms that are opened and no longer contain
+	 * the same options that they were saved with are able to properly display and edit existing data
+	 * without loss.
+	 */
+	protected void addOptionIfMissing(Object initialValue) {
+		Option initialValueOption = convertToOption(initialValue);
+		if (initialValueOption != null) {
+			boolean optionFound = false;
+			if (options != null) {
+				for (Option option : options) {
+					if (OpenmrsUtil.nullSafeEquals(option.getValue(), initialValueOption.getValue())) {
+						optionFound = true;
+					}
+				}
+			}
+			if (!optionFound) {
+				addOption(initialValueOption);
+			}
+		}
+	}
+	
+	/**
+	 * This method converts a generic initial value into an Option. The intention is to support all of
+	 * the current use cases that exist in the module (all of the data types that would be passed into
+	 * this method for various usages).
+	 */
+	protected Option convertToOption(Object initialValue) {
+		if (initialValue == null) {
+			return null;
+		}
+		if (initialValue instanceof OpenmrsMetadata) {
+			OpenmrsMetadata val = (OpenmrsMetadata) initialValue;
+			return new Option(HtmlFormEntryUtil.format(val), val.getId().toString(), true);
+		} else if (initialValue instanceof Concept) {
+			Concept val = (Concept) initialValue;
+			return new Option(val.getDisplayString(), val.getId().toString(), true);
+		} else if (initialValue instanceof ValueStub) {
+			ValueStub val = (ValueStub) initialValue;
+			return new Option(val.getDisplayValue(), val.getId().toString(), true);
+		} else if (initialValue instanceof OpenmrsData) {
+			OpenmrsData val = (OpenmrsData) initialValue;
+			return new Option(val.toString(), val.getId().toString(), true);
+		} else {
+			return new Option(initialValue.toString(), initialValue.toString(), true);
 		}
 	}
 	
