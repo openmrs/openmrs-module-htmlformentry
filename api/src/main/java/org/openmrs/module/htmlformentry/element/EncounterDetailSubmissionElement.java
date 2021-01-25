de44defa364e3caabe7ecc85d42a97fcc6445c26
Joel Figueiredo <joel.figueiredo@isobar.com>
2021-01-25 22:58:04 +0500

    HTML-755 Convert from client timezone to server timezone before saving on the DB
    remove unnecessary condition

--- api/src/main/java/org/openmrs/module/htmlformentry/element/EncounterDetailSubmissionElement.java
+++ api/src/main/java/org/openmrs/module/htmlformentry/element/EncounterDetailSubmissionElement.java
@@ -732,7 +732,7 @@ public class EncounterDetailSubmissionElement implements HtmlGeneratorElement, F
 				String serverTimezone = TimeZone.getDefault().getID();
 				boolean handleTimezones = Boolean.parseBoolean(
 				    Context.getAdministrationService().getGlobalProperty(HtmlFormEntryConstants.GP_HANDLE_TIMEZONES));
-				if (!serverTimezone.equals(clientTimezone) && !handleTimezones && (clientTimezone != null)) {
+				if (!serverTimezone.equals(clientTimezone) && !handleTimezones) {
 					throw new IllegalStateException("htmlformentry.error.handleTimezones");
 				}
 				
