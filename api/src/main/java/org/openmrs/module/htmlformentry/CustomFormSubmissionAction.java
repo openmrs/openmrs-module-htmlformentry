bd3ebe11bd73d4c8c1b04b10630dcd5a0f9dceb4
mgoodrich <mark.goodrich@live.com>
2021-09-21 18:48:08 -0400

    update comments for CustomFormSubmissionAction

--- api/src/main/java/org/openmrs/module/htmlformentry/CustomFormSubmissionAction.java
+++ api/src/main/java/org/openmrs/module/htmlformentry/CustomFormSubmissionAction.java
@@ -2,13 +2,15 @@ package org.openmrs.module.htmlformentry;
 
 /**
  * Classes that implement this interface can be added to the FormSubmissionActions via
- * submissionActions.addCustomFormSubmissionAction(CustomFormSubmissionAction). The
- * applyAction(session) method will then be called at the end of the "applyActions" phase of form
+ * the PostSubmissionAction tag. applyAction(session) method will then be called at the end of the "applyActions" phase of form
  * submission (see FormEntrySession.applyActions()) For an example of usage, see:
- * https://github.com/PIH/openmrs-module-appointmentschedulingui/commit/e2cda8de1caa8a45d319ae4fbf7714c90c9adb8b
+ *
+ * https://github.com/PIH/openmrs-module-pihcore/blob/master/api/src/main/java/org/openmrs/module/pihcore/htmlformentry/action/ReopenVisitAction.java
+ *
+ * https://github.com/PIH/openmrs-config-pihemr/blob/master/configuration/pih/htmlforms/admissionNote.xml#L17
  */
 public interface CustomFormSubmissionAction {
-	
+
 	void applyAction(FormEntrySession session);
-	
+
 }
