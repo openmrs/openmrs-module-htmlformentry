99a8d3d89a668c9b3c20acde80c8fa0bee552b2f
Michael Seaton <mseaton@pih.org>
2021-11-03 10:09:23 -0400

    HTML-795 - HTMLForms drugorder throws a nullpointer if both drugs and discontinue reasons are specified (plus formatter changes)

--- api/src/main/java/org/openmrs/module/htmlformentry/CustomFormSubmissionAction.java
+++ api/src/main/java/org/openmrs/module/htmlformentry/CustomFormSubmissionAction.java
@@ -1,16 +1,15 @@
 package org.openmrs.module.htmlformentry;
 
 /**
- * Classes that implement this interface can be added to the FormSubmissionActions via
- * the PostSubmissionAction tag. applyAction(session) method will then be called at the end of the "applyActions" phase of form
- * submission (see FormEntrySession.applyActions()) For an example of usage, see:
- *
+ * Classes that implement this interface can be added to the FormSubmissionActions via the
+ * PostSubmissionAction tag. applyAction(session) method will then be called at the end of the
+ * "applyActions" phase of form submission (see FormEntrySession.applyActions()) For an example of
+ * usage, see:
  * https://github.com/PIH/openmrs-module-pihcore/blob/master/api/src/main/java/org/openmrs/module/pihcore/htmlformentry/action/ReopenVisitAction.java
- *
  * https://github.com/PIH/openmrs-config-pihemr/blob/master/configuration/pih/htmlforms/admissionNote.xml#L17
  */
 public interface CustomFormSubmissionAction {
-
+	
 	void applyAction(FormEntrySession session);
-
+	
 }
