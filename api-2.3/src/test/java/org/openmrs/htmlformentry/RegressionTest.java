0d3a60db07013318ad25eee7c455b1cd4eab18a9
Luis Oliveira <luis.oliveira@icrc.org>
2022-03-15 10:26:05 +0000

    HTML-788: Expand Obs tag to support form namespace and path Removed unnecessary tests.

--- api-2.3/src/test/java/org/openmrs/htmlformentry/RegressionTest.java
+++ api-2.3/src/test/java/org/openmrs/htmlformentry/RegressionTest.java
@@ -2,7 +2,6 @@ package org.openmrs.htmlformentry;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openmrs.Encounter;
 import org.openmrs.Obs;
@@ -95,52 +94,6 @@ public class RegressionTest extends BaseHtmlFormEntryTest {
     }
 
     @Test
-    @Ignore
-    public void testSingleObsGroupWithControlIdSavesNamespaceAndPath() throws Exception {
-        final Date date = new Date();
-        new RegressionTestHelper() {
-
-            @Override
-            public String getFormName() {
-                return "singleObsGroupFormWithControlId";
-            }
-
-            @Override
-            public String[] widgetLabels() {
-                return new String[]{"Date:", "Location:", "Provider:", "Weight:", "Allergy:", "Allergy Date:"};
-            }
-
-            @Override
-            public void setupRequest(MockHttpServletRequest request, Map<String, String> widgets) {
-                request.addParameter(widgets.get("Date:"), dateAsString(date));
-                request.addParameter(widgets.get("Location:"), "2");
-                request.addParameter(widgets.get("Provider:"), "502");
-                request.addParameter(widgets.get("Allergy:"), "Bee stings");
-                request.addParameter(widgets.get("Allergy Date:"), dateAsString(date));
-            }
-
-            @Override
-            public void testResults(SubmissionResults results) {
-                results.assertNoErrors();
-                results.assertEncounterCreated();
-                Encounter encounter = results.getEncounterCreated();
-                Obs obs = encounter.getObsAtTopLevel(false).iterator().next();
-                Assert.assertEquals("FakeForm.2.0/allergy-group-0", obs.getFormFieldPath());
-                Assert.assertEquals(2, obs.getGroupMembers().size());
-                Iterator<Obs> i = obs.getGroupMembers().iterator();
-                while (i.hasNext()) {
-                    Obs childObs = i.next();
-                    if (childObs.getConcept().getConceptId() == 80000) {
-                        Assert.assertEquals("FakeForm.2.0/allergy/coded-0", childObs.getFormFieldPath());
-                    } else {
-                        Assert.assertEquals("FakeForm.2.0/allergy/date-0", childObs.getFormFieldPath());
-                    }
-                }
-            }
-        }.run();
-    }
-
-    @Test
     public void testSingleObsGroupWithoutControlIdShouldNotCreateNamespaceAndPath() throws Exception {
         final Date date = new Date();
         new RegressionTestHelper() {
@@ -176,68 +129,6 @@ public class RegressionTest extends BaseHtmlFormEntryTest {
                 while (i.hasNext()) {
                     Obs childObs = i.next();
                     Assert.assertNull(childObs.getFormFieldPath());
-                }
-            }
-        }.run();
-    }
-
-    @Test
-    @Ignore
-    public void testNestedObsGroupWithControlIdSavesNamespaceAndPath() throws Exception {
-        final Date date = new Date();
-        new RegressionTestHelper() {
-
-            @Override
-            public String getFormName() {
-                return "nestedObsGroupFormWithControlId";
-            }
-
-            @Override
-            public String[] widgetLabels() {
-                return new String[]{"Date:", "Location:", "Provider:", "Allergy:", "Allergy Date:", "Another Allergy:",
-                        "Another Allergy Date:"};
-            }
-
-            @Override
-            public void setupRequest(MockHttpServletRequest request, Map<String, String> widgets) {
-                request.addParameter(widgets.get("Date:"), dateAsString(date));
-                request.addParameter(widgets.get("Location:"), "2");
-                request.addParameter(widgets.get("Provider:"), "502");
-                request.addParameter(widgets.get("Allergy:"), "Bee stings");
-                request.addParameter(widgets.get("Allergy Date:"), dateAsString(date));
-                request.addParameter(widgets.get("Another Allergy:"), "Poison Ivy");
-                request.addParameter(widgets.get("Another Allergy Date:"), dateAsString(date));
-            }
-
-            @Override
-            public void testResults(SubmissionResults results) {
-                results.assertNoErrors();
-                results.assertEncounterCreated();
-                Encounter encounter = results.getEncounterCreated();
-                Obs obs = encounter.getObsAtTopLevel(false).iterator().next();
-                Assert.assertEquals("FakeForm.2.0/allergy-0", obs.getFormFieldPath());
-                Assert.assertEquals(3, obs.getGroupMembers().size());
-                Iterator<Obs> i = obs.getGroupMembers().iterator();
-                while (i.hasNext()) {
-                    Obs childObs = i.next();
-                    if (childObs.getConcept().getConceptId() == 80000) {
-                        Assert.assertEquals("FakeForm.2.0/allergy/coded-0", childObs.getFormFieldPath());
-                    } else if (childObs.getConcept().getConceptId() == 1119) {
-                        Assert.assertEquals("FakeForm.2.0/allergy/date-0", childObs.getFormFieldPath());
-                    } else {
-                        // should be the nested group
-                        Assert.assertEquals("FakeForm.2.0/allergy/allergy-0", childObs.getFormFieldPath());
-                        Iterator<Obs> i2 = childObs.getGroupMembers().iterator();
-                        while (i2.hasNext()) {
-                            Obs grandchildObs = i2.next();
-                            if (grandchildObs.getConcept().getConceptId() == 80000) {
-                                Assert.assertEquals("FakeForm.2.0/allergy/allergy/coded-0",
-                                        grandchildObs.getFormFieldPath());
-                            } else if (childObs.getConcept().getConceptId() == 1119) {
-                                Assert.assertEquals("FakeForm.2.0/allergy/allergy/date-0", grandchildObs.getFormFieldPath());
-                            }
-                        }
-                    }
                 }
             }
         }.run();
